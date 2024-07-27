f63eea08a0101fd400b83ae8de70e479de102cfe
AuroraLantean <raymondlieu.crypto@runbox.com>
2024-07-27 15:47:02 +0300

    update minor changes

--- solana-nextjs/components/counter-state.tsx
+++ solana-nextjs/components/counter-state.tsx
@@ -1,33 +1,34 @@
 import { useEffect, useState } from "react";
 import { useConnection } from "@solana/wallet-adapter-react";
-import { program, counterPDA, CounterData } from "../anchor/setup";
+import { program, counterPDA, type CounterData } from "../anchor/setup";
 
 export default function CounterState() {
-  const { connection } = useConnection();
-  const [counterData, setCounterData] = useState<CounterData | null>(null);
+	const { connection } = useConnection();
+	const [counterData, setCounterData] = useState<CounterData | null>(null);
 
-  useEffect(() => {
-    // Fetch initial account data
-    program.account.counter.fetch(counterPDA).then((data) => {
-      setCounterData(data);
-    });
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		// Fetch initial account data
+		program.account.counter.fetch(counterPDA).then((data) => {
+			setCounterData(data);
+		});
 
-    // Subscribe to account change
-    const subscriptionId = connection.onAccountChange(
-      counterPDA,
-      (accountInfo) => {
-        setCounterData(
-          program.coder.accounts.decode("counter", accountInfo.data)
-        );
-      }
-    );
+		// Subscribe to account change
+		const subscriptionId = connection.onAccountChange(
+			counterPDA,
+			(accountInfo) => {
+				setCounterData(
+					program.coder.accounts.decode("counter", accountInfo.data),
+				);
+			},
+		);
 
-    return () => {
-      // Unsubscribe from account change
-      connection.removeAccountChangeListener(subscriptionId);
-    };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [program]);
+		return () => {
+			// Unsubscribe from account change
+			connection.removeAccountChangeListener(subscriptionId);
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [program]);
 
-  return <p className="text-lg">Count: {counterData?.count?.toString()}</p>;
+	return <p className="text-lg">Count: {counterData?.count?.toString()}</p>;
 }
--- solana-nextjs/components/send-sol.tsx
+++ solana-nextjs/components/send-sol.tsx
@@ -1,40 +1,48 @@
-import { web3 } from '@coral-xyz/anchor';
-import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
-import React, { FC, useCallback } from 'react';
+import { web3 } from "@coral-xyz/anchor";
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import React, { type FC, useCallback } from "react";
 
 export const SendSOL: FC = () => {
-    const { connection } = useConnection();
-    const { publicKey, sendTransaction } = useWallet();
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
 
-    const onClick = useCallback(async () => {
-        if (!publicKey) throw new WalletNotConnectedError();
+	const onClick = useCallback(async () => {
+		if (!publicKey) throw new WalletNotConnectedError();
 
-        // 890880 lamports as of 2022-09-01
-        const lamports = await connection.getMinimumBalanceForRentExemption(0);
+		// 890880 lamports as of 2022-09-01
+		const lamports = await connection.getMinimumBalanceForRentExemption(0);
 
-        const transaction = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: publicKey,
-                toPubkey: new web3.PublicKey("5a8Mejdc5L3vgMDoc8BNFWuMTHDV9xm6ARbfTkk7KZBw"),
-                lamports,
-            })
-        );
+		const transaction = new Transaction().add(
+			SystemProgram.transfer({
+				fromPubkey: publicKey,
+				toPubkey: new web3.PublicKey(
+					"5a8Mejdc5L3vgMDoc8BNFWuMTHDV9xm6ARbfTkk7KZBw",
+				),
+				lamports,
+			}),
+		);
 
-        const {
-            context: { slot: minContextSlot },
-            value: { blockhash, lastValidBlockHeight }
-        } = await connection.getLatestBlockhashAndContext();
+		const {
+			context: { slot: minContextSlot },
+			value: { blockhash, lastValidBlockHeight },
+		} = await connection.getLatestBlockhashAndContext();
 
-        const signature = await sendTransaction(transaction, connection, { minContextSlot });
+		const signature = await sendTransaction(transaction, connection, {
+			minContextSlot,
+		});
 
-        await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
-    }, [publicKey, sendTransaction, connection]);
+		await connection.confirmTransaction({
+			blockhash,
+			lastValidBlockHeight,
+			signature,
+		});
+	}, [publicKey, sendTransaction, connection]);
 
-    return (
-        <button onClick={onClick} disabled={!publicKey}>
-            Send SOL to an address!
-        </button>
-    );
+	return (
+		<button type="button" onClick={onClick} disabled={!publicKey}>
+			Send SOL to an address!
+		</button>
+	);
 };
